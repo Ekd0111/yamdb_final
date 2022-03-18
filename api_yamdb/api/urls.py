5d3443bd0575012c0ab69f805c64a97f7361e439
katrin_K <ekd0111@bk.ru>
2022-03-18 17:14:13 +0300

    commit after review4(3)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,28 +1,33 @@
 from django.urls import include, path
+from rest_framework import routers
+from users.views import APISignup, APIToken, UserViewSet
 
-from rest_framework.routers import SimpleRouter
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet)
 
-from .views import (TitleViewSet, CategoryViewSet, GenreViewSet,
-                    RegisterNewUserAPIView, CustomJWTTokenView, UserViewSet,
-                    ReviewViewSet, CommentViewSet)
-
-
-app_name = 'api'
-
-router = SimpleRouter()
-router.register(r'titles', TitleViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'genres', GenreViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet, basename='reviews')
-router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
-                r'/comments', CommentViewSet, basename='comments')
-router.register('titles', TitleViewSet, basename='titles')
+router_v1 = routers.DefaultRouter()
+router_v1.register('titles', TitleViewSet)
+router_v1.register('genres', GenreViewSet)
+router_v1.register('categories', CategoryViewSet)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='api-review',
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='api-comment'
+)
+router_v1.register(
+    'users',
+    UserViewSet,
+    basename='user',
+)
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
-    path('v1/auth/signup/', RegisterNewUserAPIView.as_view()),
-    path('v1/auth/token/', CustomJWTTokenView.as_view()),
+    path('v1/', include(router_v1.urls), name='api_v1'),
+    path('v1/auth/token/', APIToken.as_view(), name='token_api_v1'),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup_api_v1'),
 ]
